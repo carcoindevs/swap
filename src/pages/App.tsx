bc606e826f7df51aded94b48a679b747f7edf621
TiscotSerVices <tiscot2019@gmail.com>
2021-06-01 19:31:03 +0200

    removed import of applogo and new favicon

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -11,7 +11,6 @@ import { RedirectPathToSwapOnly } from './Swap/redirects'
 import { EN, allLanguages } from '../constants/localisation/languageCodes'
 import { LanguageContext } from '../hooks/LanguageContext'
 import { TranslationsContext } from '../hooks/TranslationsContext'
-import AppLogo from '../applogo.png'
 import UserBlock from '../components/UserBlock'
 
 import useGetDocumentTitlePrice from '../hooks/useGetDocumentTitlePrice'
